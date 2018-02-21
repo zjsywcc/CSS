cee19e390b9c5ce14b25610e55cd636e63f15c94
zjsywcc <zjsywcc@163.com>
2018-02-21 15:34:45 +0800

    [add] 增加SpringBoot初始模板

---
+++ src/test/java/com/windbise/css/CssApplicationTests.java
@@ -0,0 +1,16 @@
+package com.windbise.css;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CssApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
